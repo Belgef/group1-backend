daffdda0d68cbbdab862eb0b653fac72c3093103
Mykola Girnyi <mykola.hirnyi.pz.2019@lpnu.ua>
2022-06-08 21:51:57 +0300

    Added JWT login support

---
+++ MarketplaceBackend/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using MarketplaceBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MarketplaceBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        // TODO: Use database context
+        private readonly List<User> users = new(){
+            new(){ FirstName="Jim", LastName="Carrey", Email="admin@gmail.com", Password="12345", Role = new(){ Name = "admin" } },
+            new(){ FirstName="Simon", LastName="Cowell", Email="qwerty@gmail.com", Password="55555", Role = new(){ Name = "user" } }
+        };
+
+        [HttpPost("/token")]
+        public IActionResult Token(string email, string password)
+        {
+            var identity = GetIdentity(email, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid email or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromSeconds(AuthOptions.LIFETIME)),
+                    signingCredentials: new(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+            return Ok(response);
+        }
+
+        private ClaimsIdentity GetIdentity(string email, string password)
+        {
+            User user = users.FirstOrDefault(x => x.Email == email && x.Password == password);
+            if (user != null)
+            {
+                List<Claim> claims = new(){
+                    new(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                    new(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name)
+                };
+                ClaimsIdentity claimsIdentity = new(claims, "Token", 
+                    ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+            return null;
+        }
+    }
+}
