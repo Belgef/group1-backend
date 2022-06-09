3edc817f619689eb0fb7437b366ea27c9131db02
Mykola Girnyi <mykola.hirnyi.pz.2019@lpnu.ua>
2022-06-09 17:40:57 +0300

    Added sample controller

---
+++ MarketplaceBackend/Controllers/SampleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok(new {message="Hello, " + User.Claims.First(e=>e.Type=="fname") });
+        }
+    }
+}
